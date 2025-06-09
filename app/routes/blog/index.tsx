b08c6a8d312d143883e4495ef69ce259e3e4b255
Brad Traversy <traversymedia@gmail.com>
2025-06-09 12:15:49 -0400

    Fetch Post Meta

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,4 +1,25 @@
-const BlogPage = () => {
+import type { Route } from './+types/index';
+import type { PostMeta } from '~/types';
+import { Link } from 'react-router';
+
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
+  const url = new URL('/posts-meta.json', request.url);
+  const res = await fetch(url.href);
+
+  if (!res.ok) throw new Error('Failed to fetch data');
+
+  const data = await res.json();
+
+  return { posts: data };
+}
+
+const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const { posts } = loaderData;
+
+  console.log(posts);
+
   return (
     <>
       <h2 className='text-3xl text-white font-bold mb-8'>📝 Blog</h2>
