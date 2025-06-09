72b3b05429215d0d90d164c83feb8b01fc3c49ae
Brad Traversy <traversymedia@gmail.com>
2025-06-09 12:26:18 -0400

    Display Post Meta

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,6 +1,6 @@
 import type { Route } from './+types/index';
 import type { PostMeta } from '~/types';
-import { Link } from 'react-router';
+import PostCard from '~/components/PostCard';
 
 export async function loader({
   request,
@@ -17,13 +17,13 @@ export async function loader({
 
 const BlogPage = ({ loaderData }: Route.ComponentProps) => {
   const { posts } = loaderData;
-
-  console.log(posts);
-
   return (
-    <>
+    <div className='max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900'>
       <h2 className='text-3xl text-white font-bold mb-8'>📝 Blog</h2>
-    </>
+      {posts.map((post) => (
+        <PostCard key={post.slug} post={post} />
+      ))}
+    </div>
   );
 };
 
